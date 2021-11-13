2e3d037d3229a83b04090bd1536bac1d84170421
shikharmn <shikhar_mn@iitkgp.ac.in>
2021-11-14 02:13:56 +0530

    Fixed dataloading files

--- data/collate.py
+++ data/collate.py
@@ -30,6 +30,7 @@ class BaseCollateClass(nn.Module):
                     (lr) and one high resolution (hr).
         """
         batch_size = len(batch)
+        print(batch_size)
 
         hr_transformed = [self.hr_transform(batch[i]) \
                             for i in range(batch_size)]
